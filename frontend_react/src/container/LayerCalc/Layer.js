251084372ed6a75755f85a80bbb4ff28bafa82b2
Klint Keown <klintkeown@yahoo.com>
2022-06-13 17:49:20 +0000

    File Structure Cleanup

--- frontend_react/src/container/LayerCalc/Layer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layer = () => {
-  return (
-    <div>Layer</div>
-  )
-}
-
-export default Layer
--- frontend_react/src/container/ShrinkCalc/Shrink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Shrink = () => {
-  return (
-    <div>Shrink</div>
-  )
-}
-
-export default Shrink
