e7d8dd9ba9e65c5875f7fffb94e22beafd6b19a5
Klint Keown <klintkeown@yahoo.com>
2022-06-13 18:03:23 +0000

    deploy settings

--- frontend_react/src/components/index.js
+++
@@ -1,1 +0,0 @@
-export {default as BoardFormComponent} from './BoardForm';
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { BoardForm } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <BoardForm />
+    </div>
+  );
+}
+
+export default App;
